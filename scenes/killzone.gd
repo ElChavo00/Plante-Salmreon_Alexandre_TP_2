10ba0d52b270c66507608c31c3cf2297d1e8fb55
ElChavo00 <142917881+ElChavo00@users.noreply.github.com>
2025-10-16 18:12:41 -0400

    Add Gerudo Valley music track to the project
    - Imported "Gerudo Valley.mp3" as an AudioStreamMP3 resource.
    - Created a new scene "musique.tscn" with an AudioStreamPlayer2D node to play the track.
    - Set the volume and autoplay properties for the music player.

--- scenes/killzone.gd
+++ scenes/killzone.gd
@@ -3,8 +3,4 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D):
-	print("You Died")
-	timer.start()
-
-func _on_timer_timeout():
 	get_tree().reload_current_scene()
