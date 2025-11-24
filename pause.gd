3fa9ab20b1e28536bf643cb928599d90d32b5c2c
ElChavo00 <142917881+ElChavo00@users.noreply.github.com>
2025-11-23 19:35:49 -0500

    Implement pause functionality with UI and shader effects

---
+++ pause.gd
@@ -0,0 +1,33 @@
+extends Control
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
+
+
+
+func _process(delta):
+	testEsc()
+
+
+func _on_resumer_pressed():
+	resume()
+
+
+func _on_quitter_pressed():
+	get_tree().quit()
+
+
+func _on_recommencer_pressed():
+	get_tree().reload_current_scene()
